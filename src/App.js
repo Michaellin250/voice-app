e6c0b790a3c41162b2f0e25f0e2e94e163743708
Jonathan Chen <jonathanchen0510@gmail.com>
2021-07-20 14:41:03 -0400

    minor

--- src/App.js
+++ src/App.js
@@ -107,30 +107,30 @@ import ThankYouPage from "ThankYouPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-//export default function App() {
+export function App() {
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
   // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
 
 
-  //return (
-    //<Router>
-      //<Switch>
-        //<Route path="/components/:type/:subtype/:name">
-          //<ComponentRenderer />
-        //</Route>
-        //<Route path="/components/:type/:name">
-          //<ComponentRenderer />
-        //</Route>
-        //<Route path="/thank-you">
-          //<ThankYouPage />
-        //</Route>
-        //<Route path="/">
-          //<MainLandingPage />
-        //</Route>
-      //</Switch>
-    //</Router>
-  //);
-//}
+  return (
+    <Router>
+      <Switch>
+        <Route path="/components/:type/:subtype/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/components/:type/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/thank-you">
+          <ThankYouPage />
+        </Route>
+        <Route path="/">
+          <MainLandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 export default EventLandingPage;
 // export default HotelTravelLandingPage;
