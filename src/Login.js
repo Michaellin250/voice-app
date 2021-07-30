7b9179e0f92eaeaf45a30f85652af8bfe75d0686
Jonathan Chen <jonathanchen0510@gmail.com>
2021-07-30 18:56:35 -0400

    login page

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,45 @@
-import React from "react";
-   import {Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
 
-    function Login() {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        return (
-          <div>
-            <p>
-              This is the login page.
-              <br />
-              Welcome!
-            </p>
-          </div>
-        );
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
 
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
-    export default Login;
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group><br/>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <br/>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
